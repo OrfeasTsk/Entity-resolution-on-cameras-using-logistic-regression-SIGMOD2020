f4fd34db3ad1dd6ad3a97f77cbfd6e3cadc1a433
OrfeasTsk <73546575+OrfeasTsk@users.noreply.github.com>
2020-10-29 20:12:54 +0200

    Create main.c
    Main added

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+
+int main(int argc, char* argv[]){
+	int i;
+	char* datasetX=NULL,*datasetW=NULL,*tmpdir1,*json;
+	DIR* dir_ptr1,*dir_ptr2;
+	FILE* fd;
+	struct dirent* dirent_ptr;
+	
+	
+	if(argc != 5){
+		printf("Argument error\n");
+			return 1;
+	}
+	
+	for(i = 1 ; i < argc ; i += 2){ //Den mas endiaferei h seira pou dinontai kai den epitrepei tis epanalhpseis twn parametrwn
+
+		if(!strcmp("-x",argv[i]) && datasetX == NULL)
+			datasetX=argv[i+1];
+		else if(!strcmp("-w",argv[i]) && datasetW == NULL)
+			datasetW=argv[i+1];
+		else{
+			printf("Argument error\n");
+			return 1;
+		}
+	}
+	
+	
+	dir_ptr1 = opendir(datasetX);
+	while(dirent_ptr = readdir(dir_ptr1)) //Diavasma twn katalogwn
+		if(strcmp(dirent_ptr->d_name,".")  && strcmp(dirent_ptr->d_name,"..")){
+			tmpdir1 = (char*)malloc(strlen(datasetX) + strlen(dirent_ptr->d_name) + 2);
+			strcpy(tmpdir1,datasetX);
+			strcat(tmpdir1,"/");
+			strcat(tmpdir1,dirent_ptr->d_name);
+			dir_ptr2 = opendir(tmpdir1);
+			while(dirent_ptr = readdir(dir_ptr2)) //Diavasma twn json arxeiwn
+				if(strcmp(dirent_ptr->d_name,".")  && strcmp(dirent_ptr->d_name,"..")){
+					json = (char*)malloc(strlen(tmpdir1) + strlen(dirent_ptr->d_name) + 2);
+					strcpy(json,tmpdir1);
+					strcat(json,"/");
+					strcat(json,dirent_ptr->d_name); 
+					printf("%s\n",json);
+					fd=fopen(json,"r");	//Anoigma tou arxeiou hmeras
+					if(fd == NULL) 
+						return 1;
+					fclose(fd);
+				}
+			free(tmpdir1);
+			closedir(dir_ptr2);
+		}
+			
+	closedir(dir_ptr1);
+
+	return 0;
+	
+}
+
+
