6ae9c83d7e820720e48b7350a8f87439f077b152
ORFEAS TSOURAKIS <sdi1700175@linux01.di.uoa.gr>
2020-10-31 15:35:26 +0200

    condition_changed_in_parse

--- main.c
+++ main.c
@@ -66,14 +66,14 @@ int parse(char* json){
 		else{
 			if(start == -1){ //Mexri na vrethei h arxh(start)
 				if( c != ':' && c != ' '){
-					if( (c == '"' && prev != '\\') || c == '{' || c == '[')
+					if( (c == '"'  || c == '{' || c == '[') && prev != '\\')
 						push(&stack,c);
 					start = count;
 				}
 			}
 			else{
 				if(!empty(&stack)){
-					if( c == '[' || c == '{'|| (c == '"' && prev != '\\') || c == ']' || c == '}')
+					if((c == '[' || c == '{'|| c == '"'  || c == ']' || c == '}') && prev != '\\')
 						check(&stack,c);
 				}
 				else{
