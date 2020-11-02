62604199f97919155d9ab5298359acf44f74bca2
OrfeasTsk <sdi1700175@di.uoa.gr>
2020-11-02 18:07:35 +0200

    Revert "rm"
    This reverts commit 3816bef83294212ecf996c4d9c08f16aebd7fbfa.

---
+++ structs.h
@@ -0,0 +1,97 @@
+#define RED 0
+#define BLACK 1
+
+typedef struct STnode* Link;
+extern Link z;
+Link z;
+
+
+
+
+/*##################                  Start Generic Queue                                        ##########################*/
+
+struct QueueNode{
+        void* data;
+        struct QueueNode* next;
+        
+};
+
+typedef struct{
+        struct QueueNode* head;
+        struct QueueNode* tail;
+        int count;
+        
+}Queue;
+
+typedef struct{			// each node has the name of the spec and it's value
+	char* name;
+	char* value;
+}SpecNode; 
+
+typedef struct{
+	char* id;
+	Queue specs;
+	
+}Item;
+
+void QueueInit(Queue* queue);
+void QueueInsert(Queue* queue,void ** data);
+
+/*##################                  End Generic Queue                                        ##########################*/
+
+/*##################               Start Stack For only one char !! Used in Json Parse !!                        ##########################*/
+
+
+
+struct StackNode{ //Komvos listas
+	char data;
+	struct StackNode* next;
+};
+
+typedef struct LinkedStack{ //Domh listas
+	struct StackNode* head;
+	int count;
+}Stack;
+
+
+void StackInit(Stack * stack);
+struct StackNode * push(Stack * stack, char data);
+void check(Stack * stack, char c);
+int empty(Stack * stack);
+
+/*##################                  End Stack                           ##########################*/
+
+/*##################                  Start of red-black trees                           ##########################*/
+
+
+typedef struct{ //Antikeimeno komvou tou red-black tree
+	int id;
+	Queue queue;
+}RBItem;
+
+struct STnode{ 
+	RBItem* rbitem; 
+	Link l, r; 
+	int color; 
+};
+
+void treeprint(Link , int,int,Link);
+Link rotR(Link);
+Link rotL(Link);
+Link NEW(int ,Pair** ,Link, Link, int);
+void RBinit();
+void RBdestr();
+void STinit(Link*);
+//void STdestr(Link*);
+Link MakeRBTree(Link,Link);
+Link insertR(Link,int,Pair**,Link);
+Link STinsertR(Link,int,Pair** );
+
+
+/*##################                  End of red-black trees                           ##########################*/
+
+
+typedef struct{
+	Item* item;
+	Queue related; 
+}Pair;
