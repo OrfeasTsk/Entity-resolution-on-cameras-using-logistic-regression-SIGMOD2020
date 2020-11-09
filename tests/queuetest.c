8f09857ea52cee92aa59715b1ac4ad1dddf7b257
OrfeasTsk <73546575+OrfeasTsk@users.noreply.github.com>
2020-11-09 17:43:14 +0200

    test dir added

---
+++ tests/queuetest.c
@@ -0,0 +1,38 @@
+#include "acutest.h"
+#include "structs.h"
+
+
+
+void test_qcreate(void){
+
+	Queue queue;
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Pinakas me tests
+TEST_LIST = {
+	{ "queue_create", test_qcreate },
+	{ "queue_insert_next", test_qinsert },
+	/*{ "list_remove_next", test_remove_next },
+	{ "list_find", test_find },
+	{ "list_find_node", test_find_node },*/
+	{ NULL, NULL } // NULL sto telos
+};
